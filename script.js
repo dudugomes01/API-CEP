4e7a97ede8f6ae88104d6fff8df0aa0644671028
Carlos Eduardo Gomes <85941223+dudugomes01@users.noreply.github.com>
2022-08-03 16:04:13 -0300

    API-CEP
    Primeiros passos JS

---
+++ script.js
@@ -0,0 +1,12 @@
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro){
+        throw Error('Esse CEP não existe!')
+    }else
+    console.log(r)
+})
+.catch(erro =>console.log(erro))
+
+
+console.log(consultaCEP)
